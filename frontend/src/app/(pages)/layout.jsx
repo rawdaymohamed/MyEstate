3849131929f952f0dbcd0232e4fbc7abef5f0992
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-15 22:48:44 +0200

    Feat: Add map display

--- frontend/src/app/(pages)/layout.jsx
+++ frontend/src/app/(pages)/layout.jsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <div className="h-screen flex flex-col">
       <ResponsiveNav />
-      <div className="">{children}</div>
+      {children}
     </div>
   );
 }
--- frontend/src/app/(pages)/list/page.jsx
+++ frontend/src/app/(pages)/list/page.jsx
@@ -1,9 +1,37 @@
+"use client";
+import React, { useMemo, useRef, useState } from "react";
 import Card from "@/app/components/Card/Card";
 import Filter from "@/app/components/Filter/Filter";
+// import Map from "@/app/components/Map/Map";
 import { listData } from "@/app/lib/dummyData";
-import React from "react";
-
+import { locations } from "@/app/lib/location";
+import dynamic from "next/dynamic";
 const Page = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedLocationId, setSelectedLocationId] = useState(null);
+  const mapRef = useRef();
+  const handleLocationClick = (location) => {
+    setSelectedLocationId(location.id);
+    if (mapRef.current) {
+      mapRef.current.flyTo(location.coordinates, 15); // Fly to the selected location
+    }
+  };
+
+  const filteredLocations = locations.filter(
+    (location) =>
+      location.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      location.address.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("@/app/components/Map/Map"), {
+        loading: () => <p>A map is loading</p>,
+        ssr: false,
+      }),
+    []
+  );
+
   return (
     <div className="pt-[12vh] w-[98%] lg:w-[80%] xl:w-[70%] mx-auto flex flex-col xl:flex-row gap-6 p-6">
       {/* List Container */}
@@ -17,8 +45,12 @@ const Page = () => {
       </div>
 
       {/* Map Container */}
-      <div className="lg:flex-[2] h-[400px] bg-gray-200 shadow-md rounded-xl flex items-center justify-center">
-        <span className="text-gray-500">Map Placeholder</span>
+      <div className="lg:flex-[2] h-full  rounded-xl flex items-center justify-center">
+        <Map
+          locations={filteredLocations} // Pass filtered locations to the map
+          mapRef={mapRef}
+          selectedLocationId={selectedLocationId}
+        />
       </div>
     </div>
   );
