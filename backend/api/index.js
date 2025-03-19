dadb492345e73d4a49f9815d8daebac2c2a228d4
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-19 19:33:38 +0200

    Feat: Deploy to vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,7 +4,9 @@ const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.json());
 
-
+app.use("/api/test", (req, res) => {
+    return res.send("It works");
+})
 app.use((_err, _req, res, _next) => {
     res.status(500).json({
         status: 'Failed',
