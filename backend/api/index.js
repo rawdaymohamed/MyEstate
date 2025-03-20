7eb3d923a9967cf984933d64e559c2576b4702a3
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-20 20:35:44 +0200

    Fix: Fix cors config in vercel api file

--- backend/api/index.js
+++ backend/api/index.js
@@ -9,6 +9,7 @@ dotenv.config()
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
