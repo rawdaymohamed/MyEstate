603a8f7ea9c359ba7de8335988cedfa29abff8a4
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-20 20:09:54 +0200

    Chore: Add cors and render register error message

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,12 @@ import postRoutes from "./routes/post.route.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config()
+
 const app = express();
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/posts", postRoutes);
--- frontend/src/app/register/page.jsx
+++ frontend/src/app/register/page.jsx
@@ -1,20 +1,43 @@
 "use client";
+import axios from "axios";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const RegisterPage = () => {
-  const handleSubmit = (e) => {
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    console.log(formData);
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    try {
+      const res = await axios.post("http://localhost:4000/api/auth/register", {
+        username,
+        email,
+        password,
+      });
+      router.push("/login");
+    } catch (error) {
+      setError(
+        error.response?.data?.message ||
+          "Registration failed. Please try again."
+      );
+    }
   };
+
   return (
     <div className="w-[90%] lg:w-[80%] xl:w-[70%] mx-auto flex flex-col lg:flex-row h-screen">
       {/* Login Form Container */}
       <div className="flex-1 max-w-md mx-auto flex flex-col gap-6 p-6 md:p-10 justify-center rounded-2xl shadow-lg bg-white">
         <h1 className="text-2xl md:text-3xl font-bold text-center">Welcome</h1>
         <p className="text-gray-600 text-center">Create a new Account</p>
-
+        {error && (
+          <span className="text-red-500 text-sm text-center">{error}</span>
+        )}
         <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <input
             type="text"
