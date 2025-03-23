dbe3c5f820dd6ddd3b7da92f10426d3e4e93a5fe
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-24 00:20:27 +0200

    Feat: Add user crud to the backend

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,71 @@
+import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js"
+export const get = async (req, res) => {
+    try {
+        const user = await prisma.user.findUnique({ where: { id: req.params.id } })
+        return res.status(200).json({ data: user })
+    } catch (error) {
+        return res.status(500).json({ message: "Failed to get user" })
+    }
+}
+export const edit = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const { password, avatar, ...inputs } = req.body;
+        let updatedPassword;
+
+        const authenticatedUserId = req.userId;
+
+        if (userId !== authenticatedUserId) return res.status(403).json({ message: "Unauthorized" });
+
+        if (password)
+            updatedPassword = await bcrypt.hash(password, 12)
+
+        const updatedUser = await prisma.user.update({
+            where: { id: userId },
+            data: {
+                ...inputs,
+                ...(updatedPassword && { password: updatedPassword }),
+                ...(avatar && { avatar })
+            },
+            select: {
+                id: true,
+                username: true,
+                email: true,
+            }
+        });
+        return res.status(200).json({ data: updatedUser });
+    } catch (error) {
+        return res.status(500).json({ message: "Failed to edit user" })
+    }
+}
+export const remove = async (req, res) => {
+    try {
+        const userId = req.params.id;
+
+        const authenticatedUserId = req.userId;
+
+        if (userId !== authenticatedUserId) return res.status(403).json({ message: "Unauthorized" });
+        await prisma.user.delete({ where: { id: userId } });
+        return res.status(200).json({ message: "User deleted successfully" })
+    } catch (error) {
+        return res.status(500).json({ message: "Failed to remove user" })
+    }
+}
+export const getAll = async (req, res) => {
+    try {
+        const users = await prisma.user.findMany({
+            select:
+            {
+                id: true,
+                username: true,
+                email: true,
+                avatar: true,
+                createdAt: true
+            }
+        });
+        return res.status(200).json({ data: users })
+    } catch (error) {
+        return res.status(500).json({ message: "Failed to get users" })
+    }
+}
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,3 +1,9 @@
 import { Router } from "express";
+import { edit, get, getAll, remove } from "../controllers/user.controller.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
 const router = Router();
+router.get("/:id", verifyToken, get);
+router.put("/:id", verifyToken, edit);
+router.delete("/:id", verifyToken, remove);
+router.get("/", verifyToken, getAll);
 export default router;
