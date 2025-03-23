b7b12c881bf68afef8521c2f9ef9f8cf8ef169e1
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-24 00:59:39 +0200

    Fix: fix deployment - add secure to cookies

--- backend/api/index.js
+++ backend/api/index.js
@@ -11,14 +11,7 @@ dotenv.config()
 const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use(express.json());
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true, // false for localhost
-        sameSite: 'none' // 'lax' for localhost
-    })
-);
-app.set("trust proxy", 1);
+app.use(cookieParser());
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
