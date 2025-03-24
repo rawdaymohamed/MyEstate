267d25a8431d2eb8e43632e5f08d741bbf878a95
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-24 13:30:30 +0200

    Fix: fix vercel deployment cookies

--- frontend/src/app/lib/apiRequest.js
+++ frontend/src/app/lib/apiRequest.js
@@ -2,5 +2,9 @@
 import axios from "axios";
 export const apiRequest = axios.create({
     baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
-    withCredentials: true
+    withCredentials: true,
+    credentials: 'include',
+    headers: {
+        'Content-Type': 'application/json',
+    },
 })
