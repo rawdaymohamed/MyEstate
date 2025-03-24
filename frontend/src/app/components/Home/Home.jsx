6d2e34b62560351c4d713c29b38e711c50c977f7
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-24 20:24:18 +0200

    Fix: update frontend - fix header style on home page

--- frontend/src/app/components/Home/Home.jsx
+++ frontend/src/app/components/Home/Home.jsx
@@ -11,7 +11,7 @@ const MyHome = () => {
     <div className="w-[90%] lg:w-[80%] xl:w-[70%] mx-auto flex flex-col lg:flex-row h-screen">
       {/* Text container */}
       <div className="flex-1 max-w-2xl mx-auto flex flex-col gap-6 p-6 md:p-10 justify-center">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold my-3">
           Find Your Perfect Home, Effortlessly.
         </h1>
         <p className="font-light leading-7 text-gray-600">
