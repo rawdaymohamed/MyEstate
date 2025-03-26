ee1dc0bbfa2a146f13d271c906128c5bdee4e0c0
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-26 11:25:46 +0200

    dummy commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -45,8 +45,8 @@ export const login = async (req, res) => {
             secure: isProduction,
             sameSite: isProduction ? "none" : "lax",
             path: "/",
-
-
+            // domain: isProduction ? 'rawdaymohamed-my-estate.vercel.app' : "localhost:4000",
+            // partitioned: isProduction,
             maxAge: age,
         }
         ).json({ message: "Login successful", data: userInfo });
