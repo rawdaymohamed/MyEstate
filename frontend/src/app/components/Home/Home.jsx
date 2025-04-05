20f891a8030c3d6fbdff7f70a063af5ab33027a8
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-04-05 02:17:56 +0200

    Fix: home page / style

--- frontend/src/app/components/Home/Home.jsx
+++ frontend/src/app/components/Home/Home.jsx
@@ -10,7 +10,7 @@ const MyHome = () => {
     <div className="w-[90%] lg:w-[80%] xl:w-[70%] mx-auto flex flex-col lg:flex-row h-screen">
       {/* Text container */}
       <div className="flex-1 w-[90%] sm:w-[70%] md:w-[50%] lg:max-w-md mx-auto flex flex-col gap-6 p-6 md:p-10 justify-center rounded-2xl shadow-lg bg-white">
-        <h1 className="text-2xl md:text-3xl font-bold mt-10">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mt-10">
           Find Your Perfect Home Effortlessly.
         </h1>
 
