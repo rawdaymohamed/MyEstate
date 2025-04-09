9541037408039afeb971aa0fe6e9ec227d56e8e3
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-04-09 19:22:31 +0200

    update next.config frontend - add unsplash image domain

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,50 +1,59 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
 import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
-    const { username, email, password } = req.body;
-    try {
-        // Hash password
-        const hashedPassword = await bcrypt.hash(password, 12);
-        // Create a new user
-        const newUser = await prisma.user.create({
-            data: {
-                username, email, password: hashedPassword
-            }
-        });
-        return res.status(201).json({ message: "User created successfully" });
-    } catch (err) {
-        // console.log(err);
-        return res.status(500).json({ message: "Failed to create user" })
-    }
-}
+  const { username, email, password } = req.body;
+  try {
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 12);
+    // Create a new user
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+    return res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    // console.log(err);
+    return res.status(500).json({ message: "Failed to create user" });
+  }
+};
 
 export const login = async (req, res) => {
-    try {
-        const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-        // Check user exists
-        const user = await prisma.user.findUnique({ where: { username } });
-        if (!user) return res.status(401).json({ message: "Invalid credentials" });
+    // Check user exists
+    const user = await prisma.user.findUnique({ where: { username } });
+    console.log(user);
+    if (!user) return res.status(401).json({ message: "Invalid credentials" });
 
-        // Check correct password
-        const isValidPassword = await bcrypt.compare(password, user.password);
-        if (!isValidPassword) return res.status(401).json({ message: "Invalid credentials" });
+    // Check correct password
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword)
+      return res.status(401).json({ message: "Invalid credentials" });
 
-        // Generate a JWT token
-        const age = 1000 * 60 * 60 * 24 * 7; // 7 days
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: age });
-        const { password: password_, ...userInfo } = user;
+    // Generate a JWT token
+    const age = 1000 * 60 * 60 * 24 * 7; // 7 days
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: age,
+    });
+    const { password: password_, ...userInfo } = user;
 
-        // Send the token as part of the response
-        return res.json({ message: "Login successful", token, userInfo });
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({ message: "Failed to login" });
-    }
+    // Send the token as part of the response
+    return res.json({ message: "Login successful", token, userInfo });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Failed to login" });
+  }
 };
 
 export const logout = (req, res) => {
-    return res.clearCookie("token").status(200).json({ message: "You logged out successfully" });
-}
+  return res
+    .clearCookie("token")
+    .status(200)
+    .json({ message: "You logged out successfully" });
+};
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['res.cloudinary.com'],
+        domains: ['res.cloudinary.com', 'plus.unsplash.com'],
         // loader: 'cloudinary',
         // path: 'https://res.cloudinary.com/my-username/image/upload',
         remotePatterns: [
