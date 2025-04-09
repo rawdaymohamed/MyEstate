9bb8e23a50a83fc81f94e34312919f65ed19044f
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-04-09 19:07:21 +0200

    update axios config frontend

--- frontend/src/app/lib/apiRequest.js
+++ frontend/src/app/lib/apiRequest.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance without headers
 export const apiRequest = axios.create({
-    baseURL: `/api`,
-    // baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
     withCredentials: true, // Ensure that credentials (cookies) are included in requests
 });
 
